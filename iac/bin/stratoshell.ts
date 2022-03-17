d33d554f2448120e78b6daec4b510cb28653c529
Ethan Rucinski <ethan.rucinski@gmail.com>
2022-03-16 23:21:27 -0500

    app config and image stack

--- iac/bin/stratoshell.ts
+++ iac/bin/stratoshell.ts
@@ -3,23 +3,24 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { NetworkStack } from "../lib/network-stack";
 import { DynamoStack } from "../lib/dynamo-stack";
+import { ImageStack } from "../lib/image-stack";
 
 const app = new cdk.App();
 
-// Network resources
-new NetworkStack(app, "stratoshell-network-stack", {
+const appConfig = {
     env: {
         account: process.env.CDK_DEFAULT_ACCOUNT,
         region: process.env.CDK_DEFAULT_REGION,
     },
-});
+};
+
+// Network resources
+new NetworkStack(app, "stratoshell-network-stack", appConfig);
 
 // Dynamo resources in primary region only
 if (process.env.CDK_DEFAULT_REGION == "us-east-2") {
-    new DynamoStack(app, "stratoshell-dynamo-stack", {
-        env: {
-            account: process.env.CDK_DEFAULT_ACCOUNT,
-            region: process.env.CDK_DEFAULT_REGION,
-        },
-    });
+    new DynamoStack(app, "stratoshell-dynamo-stack", appConfig);
 }
+
+// Task and images
+new ImageStack(app, "stratoshell-image-stack", appConfig);
